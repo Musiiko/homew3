f9a72b682a43ec29ff81205a0361f2ca801048b0
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-07-11 23:39:16 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k = 8;
+        int n = 1;
+        int num = 1;
+        while (n * n < k) {
+            n++;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (num <= k) {
+                    System.out.print(num + "\t");
+                    num++;
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
+
+
